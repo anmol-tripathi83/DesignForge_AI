4613295b652e31db211c86bd4101fd0fca8c508b
anmol-tripathi83 <anmoltripathi8303@gmail.com>
2026-08-03 09:16:52 +0530

    Basic Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-600">
+      <div className="bg-white p-8 rounded-xl shadow-xl">
+        <h1 className="text-4xl font-bold text-red-600">
+          Tailwind CSS is Working! 🎉
+        </h1>
+
+        <button className="mt-6 px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-700 transition">
+          Click Me
+        </button>
+      </div>
+    </div>
+  );
+}
